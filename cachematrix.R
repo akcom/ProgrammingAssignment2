dad79ed6ede2a6bef1bfc3122f2d4a8eed1c4827
Alex Kravetz <cppcoder@gmail.com>
2016-09-19 20:02:52 -0400

    solved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file is used for caching matrix inversion, which is typically an expensive operation
+## Basically what we're implementing is cached lazy evaluation of inversion
 
-## Write a short comment describing this function
-
+#create the "special" matrix object
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(matrx) {
+    x <<- matrx
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(matrx.inv) inv <<- matrx.inv
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function solves the matrix inversion if it has not been done already
+## OR pulls the inversion from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if (is.null(inv)) {
+    inv <- solve(x$get())
+    x$setinv(inv)
+  }
+  return(inv)
 }
+
+#test it
+#cache <- makeCacheMatrix(matrix(runif(25), 5, 5))
+#cacheSolve(cache)
